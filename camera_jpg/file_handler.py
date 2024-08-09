58960240bea2ecc5bdecbaa90b56ac69a0232a61
Grisha <grisha765@tuta.io>
2024-08-09 17:54:23 +0300

    [u] use async

--- camera_jpg/file_handler.py
+++ camera_jpg/file_handler.py
@@ -1,22 +1,28 @@
+import asyncio
+import aiohttp
+import os
 from pyftpdlib.handlers import FTPHandler
-import requests, os
 from config.config import Config
 
 class FileFTPHandler(FTPHandler):
-    def on_file_received(self, file):
-        with open(file, 'rb') as f:
-            file_bytes = f.read()
-        
-        response = requests.put(
-            f"{Config.ntfy_serv}{Config.ntfy_topic}",
-            data=file_bytes,
-            headers={"Title":"The camera detect movement!", "Filename": file, "Priority": "max"}
-        )
+    async def process_file_received(self, file):
+        async with aiohttp.ClientSession() as session:
+            with open(file, 'rb') as f:
+                file_bytes = f.read()
 
-        if response.status_code == 200:
-            os.remove(file)
-        else:
-            pass
+            async with session.put(
+                f"{Config.ntfy_serv}{Config.camera_topic}",
+                data=file_bytes,
+                headers={"Title": "The camera detect movement!", "Filename": file, "Priority": "max"}
+            ) as response:
+
+                if response.status == 200:
+                    os.remove(file)
+
+    def on_file_received(self, file):
+        asyncio.run(self.process_file_received(file))
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
+
+
--- core/ftp.py
+++ core/ftp.py
@@ -1,9 +1,10 @@
+import asyncio
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.servers import FTPServer
 from camera_jpg.file_handler import FileFTPHandler
 from config.config import Config
 
-def run_ftp_server():
+async def run_ftp_server():
     authorizer = DummyAuthorizer()
     authorizer.add_user(Config.ftp_user, Config.ftp_passwd, "/tmp", perm="elradfmw")
     authorizer.add_anonymous("/tmp")
@@ -12,7 +13,10 @@ def run_ftp_server():
     handler.authorizer = authorizer
 
     server = FTPServer((Config.ip, Config.port), handler)
-    server.serve_forever()
+
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, server.serve_forever)
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
+
