43fba68ee489a068c05576e26fcbe2b0011110ad
Grisha <grisha765@tuta.io>
2024-09-21 20:56:46 +0300

    [f] fix logging main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 logging.info(f"Script initialization, logging level: {Config.log_level}")
-logging.info(f"Notify link: {Config.ntfy_serv}{Config.ntfy_topic}")
+logging.info(f"Notify link: {Config.ntfy_serv}{Config.camera_topic}")
 
 async def main():
     await run_ftp_server()
